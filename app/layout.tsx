e458709e360e2e08a065e3f5f9398d7d71b110ad
Aryan Vishwakarma <aryanongithub@gmail.com>
2024-01-12 13:01:10 +0530

    feat: Added domain verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ const inter = Poppins({ subsets: ['latin'], weight: "400" })
 export const metadata: Metadata = {
   title: 'Chicken Leg Piece - Ulhas Kamathe',
   description: 'Chicken Leg Piece is a one-stop destination for food lovers who are always on the hunt for the latest and greatest food trends.',
+  verification: {
+    other: {
+      'facebook-domain-verification': '8tys5c98igs7tj8g6w42mp5mntn0co'
+    }
+  }
 }
 
 export default function RootLayout({
